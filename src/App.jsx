9559c0801ca63e136d4e3e732182c4cfad153245
TLLRSK <tullarisk@gmail.com>
2024-06-19 18:12:13 +0200

    React Query & Axios: Installed and tested

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 function App() {
+  const {response} = useQuery({
+    queryKey: ['forecast'],
+    queryFn: async() => {
+      const response = await axios.get('http://api.weatherapi.com/v1/forecast.json?key=VITE_API_KEYq=London&days=3&aqi=no&alerts=no');
+      return response;
+    } 
+  });
   return <main className="weatherapp">
     </main>
 }
