8b6cd4e82bbbc9719086b235c34199450b1f7986
TLLRSK <tullarisk@gmail.com>
2024-07-03 19:15:21 +0200

    add: currentWeather text for styling App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@
 import {
   CurrentDay,
   CityForm,
-  HourCarousel
+  HourCarousel,
 } from "./util";
 
+import { useGlobalContext } from "./context/appContext";
+
 function App() {
-  return <div className="weatherapp flex flex-col overflow-hidden rounded-[12px] flex-1 xl:m-[6%] xl:ml-[12%] xl:mr-[12%]">
+  const { currentWeather } = useGlobalContext();
+  
+  return <div className={`weatherapp ${currentWeather} flex flex-col overflow-hidden rounded-[12px] flex-1 xl:m-[6%] xl:ml-[12%] xl:mr-[12%]`}>
 
     <main className=" flex-1 flex flex-col relative">
       <CityForm/>
--- src/context/appContext.jsx
+++ src/context/appContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import useForecastData from "../hooks/useForecastData";
+import { useConditionCodes } from "../util";
 
 const GlobalContext = createContext();
 
@@ -7,11 +8,25 @@ export const useGlobalContext = () => useContext(GlobalContext);
 
 const AppContext = ({children}) => {
     const [city, setCity] = useState('London');
+    const [currentWeather, setCurrentWeather] = useState('');
     const {data, isLoading, isError} = useForecastData(city);
     
+    // Deconstructing data
     const {location, current, forecast} = data || {};
+
+    // Getting current weather text
+    const defaultData = {condition: {code: '0'}}
+    const currentData = current || defaultData;
+    const {conditionText} = useConditionCodes(currentData);
+
+    useEffect(() => {
+        if (current) {
+            setCurrentWeather(conditionText);
+        }
+    }, [current]);
+
     
-    const values = {city, setCity, location, current, forecast, isLoading, isError}
+    const values = {city, setCity, location, current, currentWeather, forecast, isLoading, isError}
 
     return <GlobalContext.Provider value={values}>
         {children}
