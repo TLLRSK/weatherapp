87f16e742f9320e04dbd21a44d7048f38e8d2a8f
TLLRSK <tullarisk@gmail.com>
2024-07-03 21:08:16 +0200

    change: tailwind config colors

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import { useGlobalContext } from "./context/appContext";
 function App() {
   const { currentWeather } = useGlobalContext();
 
-  return <div className={`weatherapp ${currentWeather} flex flex-col rounded-[12px] flex-1 xl:p-[6%]`}>
+  return <div className={`weatherapp ${currentWeather} flex flex-col flex-1 xl:p-[6%]`}>
 
     <main className=" flex-1 flex flex-col relative rounded-[12px] xl:border-2 border-dashed border-gray-200">
       <CityForm/>
